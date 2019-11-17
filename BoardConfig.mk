c4a6646d1928a83f3a60c19cd968b36ffa238efc
Mohd Faraz <androiabledroid@gmail.com>
2019-11-17 11:27:53 +0530

    heart: enable some props from G42

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,10 @@ TW_REBOOT_BOOTLOADER := true
 TW_FLASH_FROM_STORAGE := true
 TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data =ordered"
 TW_INCLUDE_NTFS_3G := true
 TW_NO_EXFAT_FUSE := true
 TWHAVE_SELINUX := true
